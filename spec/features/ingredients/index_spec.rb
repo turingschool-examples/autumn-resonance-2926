169c0a752125a9b1cfd48ea820390ba825c11150
Rich Kaht <adamselene1776@protonmail.com>
2022-10-21 15:54:33 -0600

    Add feature to sort ingredients index alphabetically by name

--- spec/features/ingredients/index_spec.rb
+++ spec/features/ingredients/index_spec.rb
@@ -1,17 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe 'ingredients index' do
+  before :each do
+    @sauce = Ingredient.create!(name:'Sauce', cost:5)
+    @peppperoni = Ingredient.create!(name:'Peppperoni', cost:8)
+    @dough = Ingredient.create!(name:'Dough', cost:2)
+  end
   describe 'As a visitor' do
     it "I see a list of all the ingredients including their name and cost" do
-      sauce = Ingredient.create!(name:'Sauce', cost:5)
-      peppperoni = Ingredient.create!(name:'Peppperoni', cost:8)
-      dough = Ingredient.create!(name:'Dough', cost:2)
-      
       visit '/ingredients'
       
-      expect(page).to have_content("#{sauce.name}: #{sauce.cost}")
-      expect(page).to have_content("#{peppperoni.name}: #{peppperoni.cost}")
-      expect(page).to have_content("#{dough.name}: #{dough.cost}")
+      expect(page).to have_content("#{@sauce.name}: #{@sauce.cost}")
+      expect(page).to have_content("#{@peppperoni.name}: #{@peppperoni.cost}")
+      expect(page).to have_content("#{@dough.name}: #{@dough.cost}")
+    end
+    
+    it "The ingredients are sorted alphabetically by name" do
+      visit '/ingredients'
+      
+      expect(@dough.name).to appear_before(@peppperoni.name)
+      expect(@peppperoni.name).to appear_before(@sauce.name)
     end
   end
 end
--- spec/models/ingredient_spec.rb
+++ spec/models/ingredient_spec.rb
@@ -10,4 +10,14 @@ RSpec.describe Ingredient, type: :model do
     it {should have_many :recipe_ingredients}
     it {should have_many(:recipes).through(:recipe_ingredients)}
   end
+  
+  describe 'class methods' do
+    it "#sort_alphabetically sorts ingredients alphabetically" do
+      sauce = Ingredient.create!(name:'Sauce', cost:5)
+      peppperoni = Ingredient.create!(name:'Peppperoni', cost:8)
+      dough = Ingredient.create!(name:'Dough', cost:2)
+      
+      expect(Ingredient.sort_alphabetically).to eq([dough, peppperoni, sauce])
+    end
+  end
 end
