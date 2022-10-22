1ca56f34576e5097f6290ed8112dc1b32fa27340
Eli Fuchsman <efuchsman@gmail.com>
2022-10-21 22:03:01 -0600

    Complete ext. 2

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,12 @@
 class RecipesController < ApplicationController
 
   def show
+
     @recipe = Recipe.find(params[:id])
+
+    if params[:ingredient_id].present?
+      ingredient = Ingredient.find(params[:ingredient_id])
+      @recipe.ingredients << ingredient
+    end
   end
 end
--- spec/features/recipes/show_spec.rb
+++ spec/features/recipes/show_spec.rb
@@ -1,21 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe "Recipe Show page" do
-  before :each do
-    @omelette = Recipe.create!(name: "Omelette", complexity: 1, genre: "Breakfast")
-    @eggs = Ingredient.create!(name: "Eggs", cost: 1)
-    @cheese = Ingredient.create!(name: "Cheese", cost: 2)
-    @mushrooms = Ingredient.create!(name: "Mushrooms", cost: 2)
-    @steak = Ingredient.create!(name: "Steak", cost: 5)
-
-    RecipeIngredient.create!(ingredient: @eggs, recipe: @omelette)
-    RecipeIngredient.create!(ingredient: @cheese, recipe: @omelette)
-    RecipeIngredient.create!(ingredient: @mushrooms, recipe: @omelette)
-    RecipeIngredient.create!(ingredient: @steak, recipe: @omelette)
-
-  end
   describe "As a visitor" do
     describe "When I visit '/recipes/:id'" do
+      before :each do
+        @omelette = Recipe.create!(name: "Omelette", complexity: 1, genre: "Breakfast")
+        @eggs = Ingredient.create!(name: "Eggs", cost: 1)
+        @cheese = Ingredient.create!(name: "Cheese", cost: 2)
+        @mushrooms = Ingredient.create!(name: "Mushrooms", cost: 2)
+        @steak = Ingredient.create!(name: "Steak", cost: 5)
+
+        RecipeIngredient.create!(ingredient: @eggs, recipe: @omelette)
+        RecipeIngredient.create!(ingredient: @cheese, recipe: @omelette)
+        RecipeIngredient.create!(ingredient: @mushrooms, recipe: @omelette)
+        RecipeIngredient.create!(ingredient: @steak, recipe: @omelette)
+      end
+
       it "Then I see the recipe's name, complexity and genre, and I see a list of the names of the ingredients for the recipe." do
         visit "/recipes/#{@omelette.id}"
         # save_and_open_page
@@ -33,6 +33,31 @@ RSpec.describe "Recipe Show page" do
         visit "/recipes/#{@omelette.id}"
         # save_and_open_page
         expect(page).to have_content("Total Cost: 10")
+      end
+
+      it "Then I see a form to add an ingredient to this recipe" do
+        bacon = Ingredient.create!(name: "Bacon", cost: 3)
+        visit "/recipes/#{@omelette.id}"
+        # save_and_open_page
+        expect(page).to have_content("Add an Ingredient")
+        expect(page).to have_content("Ingredient id")
+        expect(page).to have_button("Submit")
+      end
+
+      describe "When I fill in a field And I click submit," do
+        it "I am redirected to the recipe's show page/ingredient" do
+          bacon = Ingredient.create!(name: "Bacon", cost: 3)
+
+          visit "/recipes/#{@omelette.id}"
+          # save_and_open_page
+          fill_in(:ingredient_id, with: bacon.id)
+          click_button "Submit"
+
+          expect(current_path).to eq("/recipes/#{@omelette.id}")
+          # save_and_open_page
+          expect(page).to have_content("Bacon")
+          expect(page).to have_content("Total Cost: 13")
+        end
       end
     end
   end
