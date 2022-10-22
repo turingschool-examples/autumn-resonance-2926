0ca4ba82d4d1c862960c770b470718ba3bb449b6
Joseph Hilby <joseph.hilby@gmail.com>
2022-10-22 10:45:10 -0500

    Add model test for new recipe method

--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -11,4 +11,20 @@ RSpec.describe Recipe, type: :model do
     it {should have_many :recipe_ingredients}
     it {should have_many(:ingredients).through(:recipe_ingredients)}
   end
+
+  describe '#total_cost' do
+    before(:each) do
+      @carrots = Ingredient.create!(name: "Carrots", cost: 2)
+      @onions = Ingredient.create!(name: "Onions", cost: 3)
+      @cellery = Ingredient.create!(name: "Cellery", cost: 3)
+      @stock = Recipe.create!(name: "Stock", complexity: 2, genre: "basic")
+      RecipeIngredient.create!(recipe: @stock, ingredient: @carrots)
+      RecipeIngredient.create!(recipe: @stock, ingredient: @onions)
+      RecipeIngredient.create!(recipe: @stock, ingredient: @cellery)
+    end
+    it 'will get the sum cost of all ingredients in a recipe' do
+      number = (@carrots.cost + @onions.cost + @cellery.cost)
+      expect(@stock.total_cost).to eq(number)
+    end
+  end
 end
