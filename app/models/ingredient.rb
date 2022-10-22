1be7676b5a66fad2241af984c80c05b460fe915c
Joseph Hilby <joseph.hilby@gmail.com>
2022-10-22 10:12:22 -0500

    Fix many-to-many set up

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,2 +1,8 @@
 class Ingredient < ApplicationRecord
+  has_many :recipe_ingredients
+  has_many :recipes, through: :recipe_ingredients
+
+  validates :name, presence: true
+  validates :cost, presence: true
+
 end
