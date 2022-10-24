c036e028f2694bdb9a904f047402a78bb85e498d
Darby Smith <newman.darby2@gmail.com>
2022-10-24 14:12:59 -0400

    WIP: adding ingredients cost to view

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,8 +2,11 @@ class RecipesController < ApplicationController
   def show
     @recipe = Recipe.find(params[:id])
     recipe_ingredients = RecipeIngredient.where("recipe_id = #{@recipe.id}")
-    @ingredients = recipe_ingredients.flat_map do |recipe|
+    @ingredients = recipe_ingredients.map do |recipe|
       Ingredient.where(id: recipe.ingredient_id)
     end
+    @ingredients_cost = recipe_ingredients.map do |recipe|
+      Ingredient.where(id: recipe.ingredient_id).sum(:cost)
+    end.sum
   end
 end
