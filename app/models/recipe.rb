588fc928d88e20534abbbb8526b504ef182742b5
Annie Pulzone <ajpulzone@gmail.com>
2022-10-24 09:42:08 -0700

    Add relationships

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,9 @@
 class Recipe < ApplicationRecord
+
+  validates :name, presence: true
+  validates :complexity, presence: true
+  validates :genre, presence: true
+
+  has_many :recipe_ingredients
+  has_many :ingredients, through: :recipe_ingredients
 end
