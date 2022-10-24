f7f5dc5b5166c6803db88a19589c4b84777610f2
Amanda Ross <amikaross@gmail.com>
2022-10-24 11:02:08 -0600

    Test: Write test for the ingredients index sorted alphabetically by name

--- spec/features/ingredients/index_spec.rb
+++ spec/features/ingredients/index_spec.rb
@@ -16,6 +16,17 @@ RSpec.describe "Ingredients index page" do
           expect(page).to have_content("Pecorino Cheese: 10")
         end
       end
+
+      it "displays the ingredients sorted alphabetically by name" do 
+        cheese = Ingredient.create!(name: "Pecorino Cheese", cost: 10)
+        pasta = Ingredient.create!(name: "Rigatoni Pasta", cost: 2)
+        avocado = Ingredient.create!(name: "Avocados", cost: 12)
+
+        visit "/ingredients"
+
+        expect("Avocados").to appear_before("Pecorino Cheese")
+        expect("Pecorino Cheese").to appear_before("Rigatoni Pasta")\
+      end
     end
   end
 end
