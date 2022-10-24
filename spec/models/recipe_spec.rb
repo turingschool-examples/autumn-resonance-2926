6cb88b38bb317fca49167fecf9401941adcd3b54
Amanda Ross <amikaross@gmail.com>
2022-10-24 10:54:59 -0600

    Test: add model test for total_cost instance method

--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -11,4 +11,26 @@ RSpec.describe Recipe, type: :model do
     it {should have_many :recipe_ingredients}
     it {should have_many(:ingredients).through(:recipe_ingredients)}
   end
+
+  describe 'instance methods' do 
+    describe '#total_cost' do 
+      it 'returns the total cost of all ingredients in a recipe' do 
+        cheese = Ingredient.create!(name: "Pecorino Cheese", cost: 1)
+        pasta = Ingredient.create!(name: "Penne Pasta", cost: 2)
+        tomatoes = Ingredient.create!(name: "Tomatoes", cost: 3)
+        pasta_recipe = Recipe.create!(name: "Pasta Marinara", complexity: 1, genre: "Italian")
+
+        RecipeIngredient.create!(recipe: pasta_recipe, ingredient: cheese)
+        RecipeIngredient.create!(recipe: pasta_recipe, ingredient: pasta)
+        RecipeIngredient.create!(recipe: pasta_recipe, ingredient: tomatoes)
+
+        expect(pasta_recipe.total_cost).to eq(6)
+
+        mushrooms = Ingredient.create!(name: "Mushrooms", cost: 4)
+        RecipeIngredient.create!(recipe: pasta_recipe, ingredient: mushrooms)
+
+        expect(pasta_recipe.total_cost).to eq(10)
+      end
+    end
+  end
 end
