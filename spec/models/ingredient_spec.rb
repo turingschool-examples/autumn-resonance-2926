914c7c46a563447d72efd6b2179a5dba949d7814
Kevin Ta <kvnta@yahoo.com>
2022-10-24 14:06:46 -0600

    Had to move my model test to ingredients model spec for total cost.

--- spec/models/ingredient_spec.rb
+++ spec/models/ingredient_spec.rb
@@ -10,4 +10,18 @@ RSpec.describe Ingredient, type: :model do
     it {should have_many :recipe_ingredients}
     it {should have_many(:recipes).through(:recipe_ingredients)}
   end
+
+  describe 'Active record method for total cost' do 
+
+    before :each do 
+
+      @recipe1= Recipe.create!(name: "Gorden Ramsey's 5 Star Fish", complexity: 9, genre: "Seafood")
+      @ingr3 = @recipe1.ingredients.create!(name: "Rancid Fish", cost: 20)
+      @ingr4 = @recipe1.ingredients.create!(name: "Bar Matt Tequila", cost: 100)
+    end
+    it 'Adds up total cost of all the ingredients in a recipe' do 
+
+      expect(Ingredient.total_cost).to eq(120)
+    end
+  end
 end
