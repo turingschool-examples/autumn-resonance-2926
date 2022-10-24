8afee210086b39b14e0226e1f94d6be9450713be
Darby Smith <newman.darby2@gmail.com>
2022-10-24 13:21:29 -0400

    Feat: recipe show page

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,9 @@
+class RecipesController < ApplicationController
+  def show
+    @recipe = Recipe.find(params[:id])
+    recipe_ingredients = RecipeIngredient.where("recipe_id = #{@recipe.id}")
+    @ingredients = recipe_ingredients.flat_map do |recipe|
+      Ingredient.where(id: recipe.ingredient_id)
+    end
+  end
+end
