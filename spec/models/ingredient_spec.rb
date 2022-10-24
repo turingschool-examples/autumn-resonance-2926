81b8db475be77d49f1754c07e8a795f55cb26f33
Emily Port <emilylport@gmail.com>
2022-10-24 16:48:07 -0600

    add method in ingredient model to sort alphabetically

--- spec/models/ingredient_spec.rb
+++ spec/models/ingredient_spec.rb
@@ -10,4 +10,15 @@ RSpec.describe Ingredient, type: :model do
     it {should have_many :recipe_ingredients}
     it {should have_many(:recipes).through(:recipe_ingredients)}
   end
+
+  describe '#alphabetical' do 
+    it 'ingredient name is sorted alphabetically' do 
+      cheese = Ingredient.create!(name: "Cheese", cost: 3)
+      salami = Ingredient.create!(name: "Salami", cost: 5)
+      olives = Ingredient.create!(name: "Black Olives", cost: 2)
+      onions = Ingredient.create!(name: "Onions", cost: 1)
+
+      expect(Ingredient.alphabetical).to eq([olives, cheese, onions, salami ])
+    end
+  end
 end
