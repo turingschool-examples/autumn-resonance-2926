e4ce8943784bff7ad177f94e9a9484788ad528af
Darby Smith <newman.darby2@gmail.com>
2022-10-24 14:58:32 -0400

    Add ingredients cost to show restaurant

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,11 +2,9 @@ class RecipesController < ApplicationController
   def show
     @recipe = Recipe.find(params[:id])
     recipe_ingredients = RecipeIngredient.where("recipe_id = #{@recipe.id}")
-    @ingredients = recipe_ingredients.map do |recipe|
+    @ingredients = recipe_ingredients.flat_map do |recipe|
       Ingredient.where(id: recipe.ingredient_id)
     end
-    @ingredients_cost = recipe_ingredients.map do |recipe|
-      Ingredient.where(id: recipe.ingredient_id).sum(:cost)
-    end.sum
+    @ingredients_cost = Recipe.total_cost(@ingredients)
   end
 end
