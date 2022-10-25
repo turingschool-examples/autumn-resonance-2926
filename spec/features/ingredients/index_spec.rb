f44e0eed5d038e33cd1dd898b14c0d95574e0a47
Madeline Mauser <madeline.mauser@gmail.com>
2022-10-25 15:08:57 -0600

    Add test for ingredients appearing in alphabetical order in index

--- spec/features/ingredients/index_spec.rb
+++ spec/features/ingredients/index_spec.rb
@@ -2,16 +2,24 @@ require 'rails_helper'
 
 RSpec.feature "Ingredients index page", type: :feature do
  describe 'when the page is visited' do
+  before :each do
+    @ingredient_1 = Ingredient.create!(name: 'Steak', cost: 10)
+    @ingredient_2 = Ingredient.create!(name: 'Tomato', cost: 3)
+    @ingredient_3 = Ingredient.create!(name: 'Carrot', cost: 2)
+  end
   it 'should display a list of all the ingredients including name and cost' do
-    ingredient_1 = Ingredient.create!(name: 'Steak', cost: 10)
-    ingredient_2 = Ingredient.create!(name: 'Tomato', cost: 3)
-    ingredient_3 = Ingredient.create!(name: 'Carrot', cost: 2)
-
     visit "/ingredients"
 
     expect(page).to have_content("Steak: 10")
     expect(page).to have_content("Tomato: 3")
     expect(page).to have_content("Carrot: 2")
+  end
+
+  it 'should display the ingredients in alphabetical order' do
+    visit "/ingredients"
+
+    expect("Carrot").to appear_before("Steak")
+    expect("Steak").to appear_before("Tomato")
   end
  end
 end
