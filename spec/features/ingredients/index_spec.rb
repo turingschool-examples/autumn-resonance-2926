73f4c9cb80c50b5c134c75f7ee06328e24f697ac
Mackenzie Leng <kenz.joy@outlook.com>
2022-10-24 23:31:52 -0600

    feat: add test for listing ingredients alphabetically and install orderly gem

--- spec/features/ingredients/index_spec.rb
+++ spec/features/ingredients/index_spec.rb
@@ -32,6 +32,27 @@ RSpec.describe '/ingredients/index.html.erb', type: :feature do
         expect(page).to have_content(feta.name)
         expect(page).to have_content(feta.cost)
       end
+
+      it '- shows all of the ingredient names in alphabetical order' do
+        ground_beef = Ingredient.create!(name: "Ground Beef", cost: 2)
+        salt = Ingredient.create!(name: "Salt", cost: 4)
+        pasta = Ingredient.create!(name: "Pasta", cost: 3)
+        tomato_paste = Ingredient.create!(name: "Tomato Paste", cost: 2)
+        butter = Ingredient.create!(name: "Butter", cost: 5)
+        chicken = Ingredient.create!(name: "Chicken", cost: 10)
+        orzo = Ingredient.create!(name: "Orzo", cost: 3)
+        feta = Ingredient.create!(name: "Feta Cheese", cost: 7)
+
+        visit '/ingredients'
+
+        expect(butter.name).to appear_before(chicken.name)
+        expect(chicken.name).to appear_before(feta.name)
+        expect(feta.name).to appear_before(ground_beef.name)
+        expect(ground_beef.name).to appear_before(orzo.name)
+        expect(orzo.name).to appear_before(pasta.name)
+        expect(pasta.name).to appear_before(salt.name)
+        expect(salt.name).to appear_before(tomato_paste.name)
+      end
     end
   end
 end
