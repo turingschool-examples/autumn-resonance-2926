24c75fc2fcf3914de07b2ebf36ff077efc4d5c44
Mackenzie Leng <kenz.joy@outlook.com>
2022-10-24 22:59:31 -0600

    passing test for instance method self.total_cost

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -5,4 +5,10 @@ class Recipe < ApplicationRecord
 
   has_many :recipe_ingredients
   has_many :ingredients, through: :recipe_ingredients
+
+  def total_cost
+    self.ingredients.sum do |ingredient|
+      ingredient.cost
+    end
+  end
 end
