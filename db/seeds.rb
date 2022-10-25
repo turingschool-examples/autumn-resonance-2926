1d63ed296c5c24cdffa97f2913fe1031a4b8b7b7
Mackenzie Leng <kenz.joy@outlook.com>
2022-10-24 21:32:03 -0600

    feat:  user story 1 complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+ground_beef = Ingredient.create!(name: "Ground Beef", cost: 2)
+salt = Ingredient.create!(name: "Salt", cost: 4)
+pasta = Ingredient.create!(name: "Pasta", cost: 3)
+tomato_paste = Ingredient.create!(name: "Tomato Paste", cost: 2)
+butter = Ingredient.create!(name: "Butter", cost: 5)
+chicken = Ingredient.create!(name: "Chicken", cost: 10)
+orzo = Ingredient.create!(name: "Orzo", cost: 3)
+feta = Ingredient.create!(name: "Feta Cheese", cost: 7)
---
+++ spec/features/ingredients/index_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+RSpec.describe '/ingredients/index.html.erb', type: :feature do 
+  describe 'as a user' do
+    describe 'when I visit the ingredients index page' do
+      it '- I see a list of all the ingredients including their name and cost' do
+        ground_beef = Ingredient.create!(name: "Ground Beef", cost: 2)
+        salt = Ingredient.create!(name: "Salt", cost: 4)
+        pasta = Ingredient.create!(name: "Pasta", cost: 3)
+        tomato_paste = Ingredient.create!(name: "Tomato Paste", cost: 2)
+        butter = Ingredient.create!(name: "Butter", cost: 5)
+        chicken = Ingredient.create!(name: "Chicken", cost: 10)
+        orzo = Ingredient.create!(name: "Orzo", cost: 3)
+        feta = Ingredient.create!(name: "Feta Cheese", cost: 7)
+
+        visit '/ingredients'
+
+        expect(page).to have_content(ground_beef.name)
+        expect(page).to have_content(ground_beef.cost)
+        expect(page).to have_content(salt.name)
+        expect(page).to have_content(salt.cost)
+        expect(page).to have_content(pasta.name)
+        expect(page).to have_content(pasta.cost)
+        expect(page).to have_content(tomato_paste.name)
+        expect(page).to have_content(tomato_paste.cost)
+        expect(page).to have_content(butter.name)
+        expect(page).to have_content(butter.cost)
+        expect(page).to have_content(chicken.name)
+        expect(page).to have_content(chicken.cost)
+        expect(page).to have_content(orzo.name)
+        expect(page).to have_content(orzo.cost)
+        expect(page).to have_content(feta.name)
+        expect(page).to have_content(feta.cost)
+      end
+    end
+  end
+end
