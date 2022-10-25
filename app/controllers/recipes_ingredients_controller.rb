4c1a15d2a711fbf3eb7efb6581964968885a58d4
James White <whitejaad@gmail.com>
2022-10-24 22:02:51 -0700

    started user 3 stop with 25 minutes to spare

--- app/controllers/recipes_ingredients_controller.rb
+++ app/controllers/recipes_ingredients_controller.rb
@@ -2,5 +2,7 @@ class RecipesIngredientsController < ApplicationController
   def show 
     @recipes = Recipe.find(params[:id])
     @ingredients = @recipes.ingredients
+    @total_cost += ingredient.cost
+    end
   end
 end
--- spec/features/ingredients/recipes/show_spec.rb
+++ spec/features/ingredients/recipes/show_spec.rb
@@ -14,7 +14,10 @@ RSpec.describe Recipe do
       ingredient2 = Ingredient.create!(name: "Pasta", cost: 3)
       ingredient3 = Ingredient.create!(name: "Salt", cost: 4)
       ingredient4 = Ingredient.create!(name: "Sauce", cost: 5)
-      ingredients = Ingredient.all
+      recipe.ingredients << ingredient1 
+      recipe.ingredients << ingredient2 
+      recipe.ingredients << ingredient3 
+      recipe.ingredients << ingredient4 
       visit "/recipes/#{recipe.id}"
 
       expect(page).to have_content(recipe.name)
@@ -28,14 +31,39 @@ RSpec.describe Recipe do
       recipe = Recipe.create!(name: "Spaghetti", complexity: 2, genre: "Italian")
       ingredient1 = Ingredient.create!(name: "Ground Beef", cost: 2)
       ingredient2 = Ingredient.create!(name: "Pasta", cost: 3)
-      ingredient2 = Ingredient.create!(name: "Salt", cost: 4)
-      ingredient2 = Ingredient.create!(name: "Sauce", cost: 5)
-      ingredients = Ingredient.all
+      ingredient3 = Ingredient.create!(name: "Salt", cost: 4)
+      ingredient4 = Ingredient.create!(name: "Sauce", cost: 5)
+      recipe.ingredients << ingredient1 
+      recipe.ingredients << ingredient2 
+      recipe.ingredients << ingredient3 
+      recipe.ingredients << ingredient4 
       visit "/recipes/#{recipe.id}"
+
+      expect(page).to have_content("Pasta")
 
     
    end
 
+# User Story 3
+# As a visitor,
+# When I visit '/recipes/:id'
+# I see the total cost of all of the ingredients in the recipe.
+# (e.g. "Total Cost: 22")
 
+   describe 'total#cost' do 
+     it 'shows the total cost of the recipe' do
+      recipe = Recipe.create!(name: "Spaghetti", complexity: 2, genre: "Italian")
+      ingredient1 = Ingredient.create!(name: "Ground Beef", cost: 2)
+      ingredient2 = Ingredient.create!(name: "Pasta", cost: 3)
+      ingredient3 = Ingredient.create!(name: "Salt", cost: 4)
+      ingredient4 = Ingredient.create!(name: "Sauce", cost: 5)
+      recipe.ingredients << ingredient1 
+      recipe.ingredients << ingredient2 
+      recipe.ingredients << ingredient3 
+      recipe.ingredients << ingredient4 
+      visit "/recipes/#{recipe.id}"
+      expect(page).to have_content("Total Cost: 14")
+    end
+  end 
 
 end
