0ba729684fd8e77079ed7f0e5b7efb047086aa94
Krisetn Nestler <kristen.nestler@gmail.com>
2022-10-25 17:44:39 -0400

    User story 2 complete

---
+++ spec/features/recipes/recipes_show_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe Recipe, type: :feature do
+  describe 'When I visit /recipes/:id' do
+    it 'has a Recipe name, genre, complexity and a list of all the ingredients' do 
+      taco = Recipe.create!(name:"Tacos", complexity: "Easy", genre: "Mexican")
+      pie = Recipe.create!(name:"Pie", complexity: "Easy", genre: "Dessert")
+
+      ground_beef = Ingredient.create!(name:"Ground Beef", cost: 2)
+      salt = Ingredient.create!(name:"Salt", cost: 4)
+      tortilla = Ingredient.create!(name:"Tortillas", cost: 3)
+      milk = Ingredient.create!(name:"Milk", cost: 5)
+
+      recipe_ingredients_1 = RecipeIngredient.create!(recipe_id: taco.id, ingredient_id: ground_beef.id)
+      recipe_ingredients_1 = RecipeIngredient.create!(recipe_id: taco.id, ingredient_id: salt.id)
+      recipe_ingredients_1 = RecipeIngredient.create!(recipe_id: taco.id, ingredient_id: tortilla.id)
+      recipe_ingredients_1 = RecipeIngredient.create!(recipe_id: pie.id, ingredient_id: milk.id)
+
+      visit "/recipes/#{taco.id}"
+
+      expect(page).to have_content("Tacos")
+      expect(page).to have_content("Salt")
+      expect(page).to have_content("Tortillas")
+      expect(page).to_not have_content("Milk")
+    end 
+  end
+
+end
--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -11,4 +11,25 @@ RSpec.describe Recipe, type: :model do
     it {should have_many :recipe_ingredients}
     it {should have_many(:ingredients).through(:recipe_ingredients)}
   end
+
+  describe 'ingredient total cost' do 
+    it "#ingredient_total" do 
+      taco = Recipe.create!(name:"Tacos", complexity: "Easy", genre: "Mexican")
+      pie = Recipe.create!(name:"Pie", complexity: "Easy", genre: "Dessert")
+
+      ground_beef = Ingredient.create!(name:"Ground Beef", cost: 2)
+      salt = Ingredient.create!(name:"Salt", cost: 4)
+      tortilla = Ingredient.create!(name:"Tortillas", cost: 3)
+      milk = Ingredient.create!(name:"Milk", cost: 5)
+
+      recipe_ingredients_1 = RecipeIngredient.create!(recipe_id: taco.id, ingredient_id: ground_beef.id)
+      recipe_ingredients_1 = RecipeIngredient.create!(recipe_id: taco.id, ingredient_id: salt.id)
+      recipe_ingredients_1 = RecipeIngredient.create!(recipe_id: taco.id, ingredient_id: tortilla.id)
+      recipe_ingredients_1 = RecipeIngredient.create!(recipe_id: pie.id, ingredient_id: milk.id)
+
+      expect(taco.ingredients_total).to eq(9)
+
+
+    end
+  end
 end
