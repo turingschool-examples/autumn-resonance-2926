86f9499d96a2521ac1d15e5dab0363562940ee1b
Madeline Mauser <madeline.mauser@gmail.com>
2022-10-25 14:01:56 -0600

    Add test for recipe show page

---
+++ spec/features/recipes/show_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.feature "Recipe show page", type: :feature do
+ describe 'when the page is visited' do
+  it 'should display a list of all the ingredients including name and cost' do
+    recipe = Recipe(name: "Steak Stew", complexity: 2, genre: "American")
+    ingredient_1 = recipe.ingredients.create!(name: 'Steak', cost: 10)
+    ingredient_2 = recipe.ingredients.create!(name: 'Tomato', cost: 3)
+    ingredient_3 = recipe.ingredients.create!(name: 'Carrot', cost: 2)
+
+    visit "/recipes/#{recipe.id}"
+
+    expect(page).to have_content("Steak Stew")
+    expect(page).to have_content("Complexity: 2")
+    expect(page).to have_content("Genre: American")
+
+    within "#ingredients" do
+      expect(page).to have_content("Steak")
+      expect(page).to have_content("Tomato")
+      expect(page).to have_content("Carrot")
+    end
+  end
+ end
+end
