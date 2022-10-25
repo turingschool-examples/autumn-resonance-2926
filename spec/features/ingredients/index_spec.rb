796bc55575e9805519a333a94b2ef1a4f6c43ee8
Gabe Nunez <gabriel.c.nunez@gmail.com>
2022-10-25 12:24:50 -0700

    Create testing for us1

---
+++ spec/features/ingredients/index_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe 'the ingredients index page' do
+  it 'lists the name and cost of each ingredient' do
+    salt = Ingredient.create!(name: 'Salt', cost: '2')
+    beef = Ingredient.create!(name: 'Ground Beef', cost: '6')
+    chicken = Ingredient.create!(name: 'Chicken Thighs', cost: '4')
+
+    visit '/ingredients'
+
+    within "#ingredients-#{salt.id}" do
+      expect(page).to have_content('Salt')
+      expect(page).to have_content('2')
+    end
+
+    within "#ingredients-#{beef.id}" do
+      expect(page).to have_content('Ground Beef')
+      expect(page).to have_content('6')
+    end
+
+    within "#ingredients-#{chicken.id}" do
+      expect(page).to have_content('Chicken Thighs')
+      expect(page).to have_content('4')
+    end
+  end
+  
+end
