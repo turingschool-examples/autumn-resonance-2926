20c1e5ce5cedd47ab787694355a053a939b5fc4f
Gabe Nunez <gabriel.c.nunez@gmail.com>
2022-10-25 13:43:14 -0700

    Leave comment in ingredients_controller re alpha sort

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,6 +1,6 @@
 class IngredientsController < ApplicationController
   def index
-    @ingredients = Ingredient.all.order(:name)
-  end
+    @ingredients = Ingredient.all.order(:name) # Should this be a class method inside
+  end                                          # the ingredients model?
   
 end
