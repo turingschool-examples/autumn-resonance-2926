c86bc6dba65c6a2e00e469b99602c10cb506bb2e
Gabe Nunez <gabriel.c.nunez@gmail.com>
2022-10-25 12:51:11 -0700

    Create testing for us2

---
+++ spec/features/recipes/show_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe 'the recipes show page' do
+  it 'lists the name and cost of each ingredient' do
+    salt = Ingredient.create!(name: 'Salt', cost: '2')
+    beef = Ingredient.create!(name: 'Ground Beef', cost: '6')
+    chicken = Ingredient.create!(name: 'Chicken Thighs', cost: '4')
+    tomato = Ingredient.create!(name: 'Tomatoes', cost: '3')
+
+    ragu = Recipe.create!(name: 'Ragu Bolognese', complexity: 3, genre: 'Italian')
+    vin = Recipe.create!(name: 'Coq Au Vin', complexity: 5, genre: 'French')
+
+    recipe_ingredient1 = RecipeIngredient.create(ingredient_id: salt.id, recipe_id: vin.id)
+    recipe_ingredient2 = RecipeIngredient.create(ingredient_id: chicken.id, recipe_id: vin.id)
+    recipe_ingredient3 = RecipeIngredient.create(ingredient_id: salt.id, recipe_id: ragu.id)
+    recipe_ingredient4 = RecipeIngredient.create(ingredient_id: beef.id, recipe_id: ragu.id)
+    recipe_ingredient5 = RecipeIngredient.create(ingredient_id: tomato.id, recipe_id: ragu.id)
+
+    visit "/recipes/#{ragu.id}"
+
+    expect(page).to have_content('Ragu Bolognese')
+    expect(page).to have_content('3')
+    expect(page).to have_content('Italian')
+    expect(page).to have_content('Ground Beef')
+    expect(page).to have_content('Tomatoes')
+    expect(page).to have_content('Salt')
+    
+    expect(page).to_not have_content('Chicken Thighs')
+  end
+end
