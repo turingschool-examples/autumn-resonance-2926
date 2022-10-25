4ba729c822043693cc13176c22215441e15bb70a
Shawn Lee <Shawnl93@hotmail.com>
2022-10-24 20:44:23 -0600

    Set up relationships and validations

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,7 @@
 class Recipe < ApplicationRecord
+  has_many :recipe_ingredients
+  has_many :ingredients, through: :recipe_ingredients
+  validates :name, presence: true
+  validates :complexity, presence: true
+  validates :genre, presence: true
 end
