4d47db3f792a766b7428a758fec2d61dc6f315d8
Gabe Nunez <gabriel.c.nunez@gmail.com>
2022-10-25 13:13:26 -0700

    Create testing for us3

--- spec/features/recipes/show_spec.rb
+++ spec/features/recipes/show_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'the recipes show page' do
-  it 'lists the name and cost of each ingredient' do
+  it 'shows the name/complexity/genre of recipe and lists each ingredient' do
     salt = Ingredient.create!(name: 'Salt', cost: '2')
     beef = Ingredient.create!(name: 'Ground Beef', cost: '6')
     chicken = Ingredient.create!(name: 'Chicken Thighs', cost: '4')
@@ -26,5 +26,21 @@ RSpec.describe 'the recipes show page' do
     expect(page).to have_content('Salt')
     
     expect(page).to_not have_content('Chicken Thighs')
+  end
+
+  it 'displays the total cost of all the ingredients in the recipe' do
+    salt = Ingredient.create!(name: 'Salt', cost: '2')
+    beef = Ingredient.create!(name: 'Ground Beef', cost: '6')
+    tomato = Ingredient.create!(name: 'Tomatoes', cost: '3')
+
+    ragu = Recipe.create!(name: 'Ragu Bolognese', complexity: 3, genre: 'Italian')
+
+    recipe_ingredient1 = RecipeIngredient.create(ingredient_id: salt.id, recipe_id: ragu.id)
+    recipe_ingredient2 = RecipeIngredient.create(ingredient_id: beef.id, recipe_id: ragu.id)
+    recipe_ingredient3 = RecipeIngredient.create(ingredient_id: tomato.id, recipe_id: ragu.id)
+
+    visit "/recipes/#{ragu.id}"
+
+    expect(page).to have_content('Total Cost: 11')
   end
 end
