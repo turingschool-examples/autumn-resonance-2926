91940fde12e7a714d10f050e1df5cd9923a62228
Madeline Mauser <madeline.mauser@gmail.com>
2022-10-25 15:27:28 -0600

    Add #order_alphabetically to ingredient model

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -5,4 +5,7 @@ class Ingredient < ApplicationRecord
   has_many :recipe_ingredients
   has_many :recipes, through: :recipe_ingredients
 
+  def self.order_alphabetically
+    order(:name)
+  end
 end
--- spec/models/ingredient_spec.rb
+++ spec/models/ingredient_spec.rb
@@ -10,4 +10,16 @@ RSpec.describe Ingredient, type: :model do
     it {should have_many :recipe_ingredients}
     it {should have_many(:recipes).through(:recipe_ingredients)}
   end
+
+  describe 'class methods' do
+    describe '#order_alphabetically' do
+      it 'should sort the ingredients alphabetically by name' do
+        ingredient_1 = Ingredient.create!(name: 'Steak', cost: 10)
+        ingredient_2 = Ingredient.create!(name: 'Tomato', cost: 3)
+        ingredient_3 = Ingredient.create!(name: 'Carrot', cost: 2)
+
+        expect(Ingredient.order_alphabetically).to eq([ingredient_3, ingredient_1, ingredient_2])
+      end
+    end
+  end
 end
