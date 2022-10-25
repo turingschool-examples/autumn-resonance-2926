6e27958b59b3f70bdc7f220c74ba32c0ccb22a09
Will Wang <willy.j.wang@gmail.com>
2022-10-25 17:56:27 -0500

    Complete user story 2

--- spec/features/ingredients/index_spec.rb
+++ spec/features/ingredients/index_spec.rb
@@ -4,11 +4,10 @@ RSpec.describe 'the ingredients index' do
   it 'lists all ingredients with their name and cost' do
     ground_beef = Ingredient.create!(name: "Ground Beef", cost: 2)
     salt = Ingredient.create(name: "Salt", cost: 4)
-    require "pry"; binding.pry
+
     visit "/ingredients"
 
     expect(page).to have_content("#{ground_beef.name}: #{ground_beef.cost}")
     expect(page).to have_content("#{salt.name}: #{salt.cost}")
-    save_and_open_page
   end
 end
--- spec/features/recipes/show_spec.rb
+++ spec/features/recipes/show_spec.rb
@@ -12,13 +12,16 @@ RSpec.describe 'the recipe show' do
   end
   it "displays a list of the names of the ingredients for the recipe" do
     pasta = Recipe.create(name: "Pasta", complexity: 2, genre: "Italian")
-    salt = pasta.ingredients.create(name: "Salt", cost: 4)
-    flour = pasta.ingredients.create(name: "Flour", cost: 3)
+    salt = Ingredient.create(name: "Salt", cost: 4)
+    flour = Ingredient.create(name: "Flour", cost: 3)
+    hot_sauce = Ingredient.create(name: "Hot Sauce", cost: 5)
+
+    RecipeIngredient.create!(ingredient: salt, recipe: pasta)
+    RecipeIngredient.create!(ingredient: flour, recipe: pasta)
 
     visit "/recipes/#{pasta.id}"
 
     expect(page).to have_content(salt.name)
     expect(page).to have_content(flour.name)
-    save_and_open_page
   end
 end
