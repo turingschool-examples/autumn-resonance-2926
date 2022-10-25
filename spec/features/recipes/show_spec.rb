df887803ed137a251a86ff348678ff83eb9286ca
Mackenzie Leng <kenz.joy@outlook.com>
2022-10-24 22:55:13 -0600

    create test for instance method recipe.total_cost

--- spec/features/recipes/show_spec.rb
+++ spec/features/recipes/show_spec.rb
@@ -43,6 +43,35 @@ RSpec.describe 'recipe show page - /recipes/:id/show.html.erb', type: :feature d
         expect(page).to_not have_content(chicken.name)
         expect(page).to_not have_content(orzo.name)
       end
+
+      it '- I see the total cost of all of the ingredients in the recipe' do
+        ground_beef = Ingredient.create!(name: "Ground Beef", cost: 2)
+        salt = Ingredient.create!(name: "Salt", cost: 4)
+        pasta = Ingredient.create!(name: "Pasta", cost: 3)
+        tomato_paste = Ingredient.create!(name: "Tomato Paste", cost: 2)
+        butter = Ingredient.create!(name: "Butter", cost: 5)
+        chicken = Ingredient.create!(name: "Chicken", cost: 10)
+        orzo = Ingredient.create!(name: "Orzo", cost: 3)
+        feta = Ingredient.create!(name: "Feta Cheese", cost: 7)
+
+        spaghetti = Recipe.create!(name: "Spaghetti", complexity: 1, genre: "Italian")
+        chicken_orzo = Recipe.create!(name: "Chicken Orzo", complexity: 3, genre: "Greek")
+
+        RecipeIngredient.create!(recipe: spaghetti, ingredient: ground_beef)
+        RecipeIngredient.create!(recipe: spaghetti, ingredient: salt)
+        RecipeIngredient.create!(recipe: spaghetti, ingredient: pasta)
+        RecipeIngredient.create!(recipe: spaghetti, ingredient: tomato_paste)
+        RecipeIngredient.create!(recipe: spaghetti, ingredient: butter)
+        RecipeIngredient.create!(recipe: chicken_orzo, ingredient: butter)
+        RecipeIngredient.create!(recipe: chicken_orzo, ingredient: salt)
+        RecipeIngredient.create!(recipe: chicken_orzo, ingredient: chicken)
+        RecipeIngredient.create!(recipe: chicken_orzo, ingredient: orzo)
+        RecipeIngredient.create!(recipe: chicken_orzo, ingredient: feta)
+
+        visit "/recipes/#{chicken_orzo.id}"
+
+        expect(
+      end
     end
   end
 end
--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -11,4 +11,36 @@ RSpec.describe Recipe, type: :model do
     it {should have_many :recipe_ingredients}
     it {should have_many(:ingredients).through(:recipe_ingredients)}
   end
+
+  describe 'instance methods' do
+    describe '#total_cost' do
+      it '-returns the total cost of a specific recipes ingredients' do 
+        ground_beef = Ingredient.create!(name: "Ground Beef", cost: 2)
+        salt = Ingredient.create!(name: "Salt", cost: 4)
+        pasta = Ingredient.create!(name: "Pasta", cost: 3)
+        tomato_paste = Ingredient.create!(name: "Tomato Paste", cost: 2)
+        butter = Ingredient.create!(name: "Butter", cost: 5)
+        chicken = Ingredient.create!(name: "Chicken", cost: 10)
+        orzo = Ingredient.create!(name: "Orzo", cost: 3)
+        feta = Ingredient.create!(name: "Feta Cheese", cost: 7)
+
+        spaghetti = Recipe.create!(name: "Spaghetti", complexity: 1, genre: "Italian")
+        chicken_orzo = Recipe.create!(name: "Chicken Orzo", complexity: 3, genre: "Greek")
+
+        RecipeIngredient.create!(recipe: spaghetti, ingredient: ground_beef)
+        RecipeIngredient.create!(recipe: spaghetti, ingredient: salt)
+        RecipeIngredient.create!(recipe: spaghetti, ingredient: pasta)
+        RecipeIngredient.create!(recipe: spaghetti, ingredient: tomato_paste)
+        RecipeIngredient.create!(recipe: spaghetti, ingredient: butter)
+        RecipeIngredient.create!(recipe: chicken_orzo, ingredient: butter)
+        RecipeIngredient.create!(recipe: chicken_orzo, ingredient: salt)
+        RecipeIngredient.create!(recipe: chicken_orzo, ingredient: chicken)
+        RecipeIngredient.create!(recipe: chicken_orzo, ingredient: orzo)
+        RecipeIngredient.create!(recipe: chicken_orzo, ingredient: feta)
+
+        expect(spaghetti.total_cost).to eq(16)
+        expect(chicken_orzo.total_cost).to eq(29)
+      end
+    end
+  end
 end
