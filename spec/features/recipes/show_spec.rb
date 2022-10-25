a8740da64e37a439f68b4e8d00455893b2a61fdf
Madeline Mauser <madeline.mauser@gmail.com>
2022-10-25 14:37:52 -0600

    Add test for recipe show page ingredient total cost

--- spec/features/recipes/show_spec.rb
+++ spec/features/recipes/show_spec.rb
@@ -2,12 +2,13 @@ require 'rails_helper'
 
 RSpec.feature "Recipe show page", type: :feature do
  describe 'when the page is visited' do
+  before :each do
+    @recipe = Recipe.create!(name: "Steak Stew", complexity: 2, genre: "American")
+    @ingredient_1 = @recipe.ingredients.create!(name: 'Steak', cost: 10)
+    @ingredient_2 = @recipe.ingredients.create!(name: 'Tomato', cost: 3)
+    @ingredient_3 = @recipe.ingredients.create!(name: 'Carrot', cost: 2)
+  end
   it 'should display a list of all the ingredients including name and cost' do
-    recipe = Recipe.create!(name: "Steak Stew", complexity: 2, genre: "American")
-    ingredient_1 = recipe.ingredients.create!(name: 'Steak', cost: 10)
-    ingredient_2 = recipe.ingredients.create!(name: 'Tomato', cost: 3)
-    ingredient_3 = recipe.ingredients.create!(name: 'Carrot', cost: 2)
-    
     visit "/recipes/#{recipe.id}"
 
     expect(page).to have_content("Steak Stew")
@@ -19,6 +20,12 @@ RSpec.feature "Recipe show page", type: :feature do
       expect(page).to have_content("Tomato")
       expect(page).to have_content("Carrot")
     end
+  end
+
+  it 'should show the total cost of all of the recipe ingredients' do
+    visit "/recipes/#{recipe.id}"
+
+    expect(page).to have_content("Total Cost: #{@ingredient_1.cost + ingredient_2.cost + ingredient_3.cost}")
   end
  end
 end
