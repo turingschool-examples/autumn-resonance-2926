fa01c9bd85d870c403d13a1cc163c15b6c087b27
Shawn Lee <Shawnl93@hotmail.com>
2022-10-24 20:49:26 -0600

    set up test for us_1

---
+++ spec/features/ingredients/index_spec.rb
@@ -0,0 +1,15 @@
+require "rails_helper"
+
+RSpec.describe "ingredients index page" do
+  before :each do
+    @ingredient_1 = Ingredient.create!(name: "Ground Beef", cost: 2)
+    @ingredient_2 = Ingredient.create!(name: "Salt", cost: 4)
+  end
+
+  it "list the ingredients including their name and cost" do
+    expect(page).to have_content(@ingredient_1.name)
+    expect(page).to have_content(@ingredient_1.cost)
+    expect(page).to have_content(@ingredient_2.name)
+    expect(page).to have_content(@ingredient_2.cost)
+  end
+end
