6531bc4a083d0bfde4fd335070fda39cd5b2db96
Mackenzie Leng <kenz.joy@outlook.com>
2022-10-24 23:15:41 -0600

    feat: passing test for user story 3

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,7 @@
 class RecipesController < ApplicationController
   def show
     @recipe = Recipe.find(params[:id])
-    @ingredients = Ingredient.all   
+    @ingredients = Ingredient.all
+    @recipe.total_cost
   end
 end
