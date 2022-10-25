c88c0905f97a5e111fb3f88233e1d1fb7085ba9f
Mostafa Sakr <msakr3@gmail.com>
2022-10-25 16:12:54 -0600

    Add test and code for recipe show page.

---
+++ spec/features/recipes/show_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe 'index' do
+  before :each do
+    @recipe_1 = Recipe.create!(name: "Burger", complexity: 2, genre: "what?")
+    @recipe_2 = Recipe.create!(name: "Lasagna", complexity: 6, genre: "why?")
+    @ingredient_1 = Ingredient.create!(name: "Ground Beef", cost: 4)
+    @ingredient_2 = Ingredient.create!(name: "Salt", cost: 1)
+    @ingredient_3 = Ingredient.create!(name: "Onions", cost: 3)
+    @ingredient_4 = Ingredient.create!(name: "Pasta", cost: 2)
+    @recipe_ingredient_1 = RecipeIngredient.create!(recipe_id: @recipe_1.id, ingredient_id: @ingredient_1.id)
+    @recipe_ingredient_2 = RecipeIngredient.create!(recipe_id: @recipe_1.id, ingredient_id: @ingredient_2.id)
+    @recipe_ingredient_3 = RecipeIngredient.create!(recipe_id: @recipe_1.id, ingredient_id: @ingredient_3.id)
+    @recipe_ingredient_4 = RecipeIngredient.create!(recipe_id: @recipe_2.id, ingredient_id: @ingredient_1.id)
+    @recipe_ingredient_5 = RecipeIngredient.create!(recipe_id: @recipe_2.id, ingredient_id: @ingredient_2.id)
+    @recipe_ingredient_6 = RecipeIngredient.create!(recipe_id: @recipe_2.id, ingredient_id: @ingredient_3.id)
+    @recipe_ingredient_7 = RecipeIngredient.create!(recipe_id: @recipe_2.id, ingredient_id: @ingredient_4.id)
+  end
+
+   it "lists all ingredients along with name and cost" do
+    visit "/recipes/#{@recipe_1.id}"
+
+    expect(page).to have_content(@recipe_1.name)
+    expect(page).to_not have_content(@recipe_2.name)
+    expect(page).to have_content(@ingredient_1.name, count: 1)
+    expect(page).to have_content(@ingredient_2.name, count: 1)
+    expect(page).to have_content(@ingredient_3.name, count: 1)
+    expect(page).to_not have_content(@ingredient_4.name)
+   end
+end
