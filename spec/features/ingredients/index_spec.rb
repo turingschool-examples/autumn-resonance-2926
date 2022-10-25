9bae3459c66403b63a5daaa97f0c3e90ae091eb6
Will Wang <willy.j.wang@gmail.com>
2022-10-25 18:28:00 -0500

    Complete extension 1

--- spec/features/ingredients/index_spec.rb
+++ spec/features/ingredients/index_spec.rb
@@ -10,4 +10,16 @@ RSpec.describe 'the ingredients index' do
     expect(page).to have_content("#{ground_beef.name}: #{ground_beef.cost}")
     expect(page).to have_content("#{salt.name}: #{salt.cost}")
   end
+  it 'lists ingredients sorted alphabetically by name' do
+    ground_beef = Ingredient.create!(name: "Ground Beef", cost: 2)
+    salt = Ingredient.create(name: "Salt", cost: 4)
+    asparagus = Ingredient.create(name: "Asparagus", cost: 6)
+    water = Ingredient.create(name: "Water", cost: 1)
+
+    visit "/ingredients"
+
+    expect("Asparagus").to appear_before("Ground Beef")
+    expect("Ground Beef").to appear_before("Salt")
+    expect("Salt").to appear_before("Water")
+  end
 end
